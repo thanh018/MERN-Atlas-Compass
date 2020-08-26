fabb0ef9e5cbe597f73ce0891016eb6e96d04d4f
Thanh Van <thanh.van@metanet.co.kr>
2020-08-26 13:44:46 +0700

    update profile get API

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,12 +1,13 @@
 const passport = require('passport');
 const router = require('express').Router();
 
+// add token (not 'Bearer') to Auth
 router.route('/').get(
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     const { user } = req;
-    const { id, name } = user;
-    res.json({ id, name });
+    const { id, username, fullname, email } = user;
+    res.json({ id, username, fullname, email });
   }
 );
 
